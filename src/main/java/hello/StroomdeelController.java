27caab6d6b56890b825b3cd16101a095c8ce79c0
Herman Polderman <herman@polderman.net>
2018-06-06 23:50:11 +0200

    gebruik api-key van deelstroom

--- src/main/java/hello/StroomdeelController.java
+++ src/main/java/hello/StroomdeelController.java
@@ -22,14 +22,18 @@ public class StroomdeelController {
 
     HttpClientBuilder clientbuilder = HttpClientBuilder.create();
     HttpHost target = new HttpHost("monitoringapi.solaredge.com", 443, "https");
-    String startDate = "2018-01-01";
+    String startDate = "2018-06-01";
     String endDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-    HttpGet getRequest = new HttpGet("/site/564256/timeFrameEnergy.json?startDate="+startDate+"&endDate="+endDate+"&api_key=NCFWGOTDJ225818AQJTFFT1W137XMCQO");
+    String siteid = "715131";
+    String api_key = "LNEPU21MZLCJSPTEDWPXAQZ09SMFXL8D";
+    String siteidpolletjes = "564256";
+    String api_key_polletjes = "NCFWGOTDJ225818AQJTFFT1W137XMCQO";
+    HttpGet getRequest = new HttpGet("/site/"+siteid+"/timeFrameEnergy.json?startDate="+startDate+"&endDate="+endDate+"&api_key="+api_key);
 
     @GetMapping("/stroomdeel")
     public String greeting(@RequestParam(name="naam", required=false, defaultValue="Herman") String naam, Model model) {
 
-        Integer stroomdelen = 10;
+        Integer stroomdelen = 1;
         Integer totaalstroomdelen = 912;
 
         model.addAttribute("naam", naam);
@@ -52,10 +56,11 @@ public class StroomdeelController {
                 Double kwh = (data.timeFrameEnergy.energy/factor/totaalstroomdelen*stroomdelen);
                 Double totaalkwh = (data.timeFrameEnergy.energy/factor);
                 model.addAttribute("unit", "kWh");
-                model.addAttribute("kwh", kwh.intValue());
-                model.addAttribute("totaalkwh", totaalkwh.intValue());
+                model.addAttribute("kwh", String.format( "%.2f", kwh ));
+                model.addAttribute("totaalkwh", String.format( "%.2f", totaalkwh));
                 model.addAttribute("startDate", startDate);
                 model.addAttribute("endDate", endDate);
+                System.out.println(model.toString());
                 return "stroomdeel";
             }
         } catch (Exception e) {
