32ab701d9773adaa1490dfbc5952e1050e781e7b
Herman Polderman <herman@polderman.net>
2018-09-02 22:31:09 +0200

    /counter endpoint serves jpg

--- src/main/java/hello/StroomdeelController.java
+++ src/main/java/hello/StroomdeelController.java
@@ -10,11 +10,16 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -32,13 +37,40 @@ public class StroomdeelController {
     String api_key = "LNEPU21MZLCJSPTEDWPXAQZ09SMFXL8D";
     String siteidpolletjes = "564256";
     String api_key_polletjes = "NCFWGOTDJ225818AQJTFFT1W137XMCQO";
-
+    Integer totaalstroomdelen = 912;
 
     @GetMapping("/stroomdeel")
     public String greeting(@RequestParam(name="naam", required=false, defaultValue="Herman") String naam,
                            @RequestParam(name="stroomdelen", required=false, defaultValue="0") Integer stroomdelen,
                            Model model) {
-        Integer totaalstroomdelen = 912;
+        try {
+            readSolarEdge(model, naam, stroomdelen);
+            return "stroomdeel";
+        } catch (Exception e) {
+            logger.error("", e);
+            return "fout";
+        }
+    }
+
+    @GetMapping("/counter")
+    public void getImageAsByteArray(HttpServletResponse response,
+                                    @RequestParam(name="naam", required=false, defaultValue="Herman") String naam,
+                                    @RequestParam(name="stroomdelen", required=false, defaultValue="0") Integer stroomdelen,
+                                    Model model) throws IOException {
+        SolarEdgeData data = readSolarEdge();
+        int factor =1;
+        switch (data.timeFrameEnergy.unit) {
+            case "Wh":
+                factor = 1000;
+                break;
+            default:
+        }
+        Double totaalkwh = (data.timeFrameEnergy.energy/factor);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        Text2Jpg.writeJpg(String.format( "%.0f", totaalkwh)+ " kwh", response.getOutputStream());
+    }
+
+    private void readSolarEdge(Model model, String naam, Integer stroomdelen) {
 
         String endDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
         HttpGet getRequest = new HttpGet("/site/"+siteid+"/timeFrameEnergy.json?startDate="+startDate+"&endDate="+endDate+"&api_key="+api_key);
@@ -46,6 +78,35 @@ public class StroomdeelController {
         model.addAttribute("naam", naam);
         model.addAttribute("stroomdelen", stroomdelen);
         model.addAttribute("totaalstroomdelen", totaalstroomdelen);
+        try {
+            SolarEdgeData data = readSolarEdge();
+            int factor =1;
+            switch (data.timeFrameEnergy.unit) {
+                case "Wh":
+                    factor = 1000;
+                    break;
+                default:
+            }
+            Double kwh = (data.timeFrameEnergy.energy/factor/totaalstroomdelen*stroomdelen);
+            Double totaalkwh = (data.timeFrameEnergy.energy/factor);
+            model.addAttribute("unit", "kWh");
+            model.addAttribute("kwh", String.format( "%.2f", kwh ));
+            model.addAttribute("totaalkwh", String.format( "%.2f", totaalkwh));
+            model.addAttribute("startDate", startDate);
+            model.addAttribute("endDate", endDate);
+            model.addAttribute("opbrengst", String.format( "%.0f", (stroomdelen*kwh)));
+            logger.info(model.toString());
+
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+    }
+
+    private SolarEdgeData readSolarEdge() {
+        String endDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        HttpGet getRequest = new HttpGet("/site/"+siteid+"/timeFrameEnergy.json?startDate="+startDate+"&endDate="+endDate+"&api_key="+api_key);
+
+
         try (CloseableHttpClient httpclient = clientbuilder.build()) {
             HttpResponse httpResponse = httpclient.execute(target, getRequest);
             HttpEntity entity = httpResponse.getEntity();
@@ -53,29 +114,13 @@ public class StroomdeelController {
                 String json = EntityUtils.toString(entity);
                 ObjectMapper m = new ObjectMapper();
                 SolarEdgeData data = m.readValue(json, SolarEdgeData.class);
-                int factor =1;
-                switch (data.timeFrameEnergy.unit) {
-                    case "Wh":
-                        factor = 1000;
-                        break;
-                    default:
-                }
-                Double kwh = (data.timeFrameEnergy.energy/factor/totaalstroomdelen*stroomdelen);
-                Double totaalkwh = (data.timeFrameEnergy.energy/factor);
-                model.addAttribute("unit", "kWh");
-                model.addAttribute("kwh", String.format( "%.2f", kwh ));
-                model.addAttribute("totaalkwh", String.format( "%.2f", totaalkwh));
-                model.addAttribute("startDate", startDate);
-                model.addAttribute("endDate", endDate);
-                model.addAttribute("opbrengst", String.format( "%.0f", (stroomdelen*kwh)));
-                logger.info(model.toString());
-                return "stroomdeel";
+                logger.info(data.toString());
+                return data;
             }
         } catch (Exception e) {
             logger.error("", e);
-            return "fout";
         }
-        return "fout";
+        return null;
     }
 
 }
